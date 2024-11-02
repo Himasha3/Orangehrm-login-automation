752716faf750e17192d6ad4e9d110f10e29104c1
Himasha3 <mashigayathri7@gmail.com>
2024-11-02 09:36:00 +0530

    add orangeHRM success test

---
+++ src/test/java/day1/LoginTest.java
@@ -0,0 +1,25 @@
+package day1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class LoginTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.manage().window().maximize();
+		 
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+		
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+	}
+
+}
